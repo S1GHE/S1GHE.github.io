d1485d4682c84e9aaad6803557cfe885411b4037
s1ghe <golikoefim65@gmail.com>
2022-12-02 22:21:14 +0300

    edd

--- app.js
+++ app.js
@@ -7,17 +7,13 @@ name_user.innerText = `${telegramAPI.initDataUnsafe.user.first_name}`;
 
 
 function valudate_from(){
-    let valid_input = [
-        document.getElementById('1').value,
-        document.getElementById('2').value,
-        document.getElementById('3').value,
-        document.getElementById('4').value,
-        document.getElementById('5').value
-    ];
-    console.log(valid_input);
-    // for(i=1; i++; i >= 12){
-    //     let valid_input = document.getElementById(`${i}`).value;
-    //     console.log(valid_input);
-    // }
+    let document1 = document.getElementById("document1").value;
+
+    if(document1 != ""){
+
+    }else{
+
+    }
+
     return false;
 }
