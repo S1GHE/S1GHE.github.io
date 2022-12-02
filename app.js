ab1e8c7e0190e4f2a97aacdb1312f0393656e478
s1ghe <golikoefim65@gmail.com>
2022-12-02 22:06:14 +0300

    edd

--- app.js
+++ app.js
@@ -2,5 +2,22 @@ let telegramAPI = window.Telegram.WebApp;
 
 telegramAPI.expand();
 
-let name_user = document.getElementById("name")
-name_user.innerText = `${telegramAPI.initDataUnsafe.user.first_name}`
+let name_user = document.getElementById("name");
+name_user.innerText = `${telegramAPI.initDataUnsafe.user.first_name}`;
+
+
+function valudate_from(){
+    let valid_input = [
+        document.getElementById('1').value,
+        document.getElementById('2').value,
+        document.getElementById('3').value,
+        document.getElementById('4').value,
+        document.getElementById('5').value
+    ];
+    console.log(valid_input);
+    // for(i=1; i++; i >= 12){
+    //     let valid_input = document.getElementById(`${i}`).value;
+    //     console.log(valid_input);
+    // }
+    return false;
+}
