66a1c1f564d49e605f226b8c50d8790ecbe7220b
s1ghe <golikoefim65@gmail.com>
2022-12-23 14:05:54 +0300

    edd

--- app.js
+++ app.js
@@ -15,8 +15,6 @@ function validate_from() {
     if (document.getElementById('document1').value != '') {
         dict_doc.document1 = document.getElementById('document1').value
         console.log(dict_doc.document1)
-        console.log(dict_doc)
-        alert(dict_doc.document1)
     }
     return false;
 }
@@ -25,6 +23,5 @@ telegramAPI.MainButton.setText('Отправить');
 telegramAPI.MainButton.show();
 
 Telegram.WebApp.onEvent('mainButtonClicked', function () {
-    alert(dict_doc.document1);
     telegramAPI.sendData(dict_doc);
 });
