6bea8e81913a783244a2118f6f017e89ef8c60f2
s1ghe <golikoefim65@gmail.com>
2022-12-23 13:12:56 +0300

    edd

--- app.js
+++ app.js
@@ -1,16 +1,14 @@
 const telegramAPI = window.Telegram.WebApp;
 telegramAPI.expand();
 
-telegramAPI.MainButton.hide();
+
 telegramAPI.MainButton.textColor = '#FFFFFF';
 telegramAPI.MainButton.color = '#198754';
 
-let array_value = [];
 
 function validate_from() {
     if (document.getElementById('document1').value != '') {
         array_value.push(document.getElementById('document1').value);
-        console.log(array_value);
     }
     return false;
 }
