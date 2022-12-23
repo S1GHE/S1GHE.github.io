2c117eb167a7fdd4cf769c14691124327d245082
s1ghe <golikoefim65@gmail.com>
2022-12-23 10:53:17 +0300

    edd

--- app.js
+++ app.js
@@ -6,42 +6,10 @@ telegramAPI.expand();
 function valudate_from() {
     if (document.getElementById('document1').value != '') {
         document.getElementById('document1-s').style.display = 'block';
+
         Telegram.WebApp.onEvent('mainButtonCliked', function () {
             telegramAPI.sendData(`${document.getElementById('document1').value}`)
         })
-        return true;
-        if (document.getElementById('document2').value == '') {
-            document.getElementById('document2-s').style.display = 'block';
-            if (document.getElementById('document3').value == '') {
-                document.getElementById('document3-s').style.display = 'block';
-                if (document.getElementById('document4').value == '') {
-                    document.getElementById('document4-s').style.display = 'block';
-                    if (document.getElementById('document5').value == '') {
-                        document.getElementById('document5-s').style.display = 'block';
-                        if (document.getElementById('document6').value == '') {
-                            document.getElementById('document6-s').style.display = 'block';
-                            if (document.getElementById('document7').value == '') {
-                                document.getElementById('document7-s').style.display = 'block';
-                                if (document.getElementById('document8').value == '') {
-                                    document.getElementById('document8-s').style.display = 'block';
-                                    if (document.getElementById('document9').value == '') {
-                                        document.getElementById('document9-s').style.display = 'block';
-                                        if (document.getElementById('document10').value == '') {
-                                            document.getElementById('document10-s').style.display = 'block';
-                                            if (document.getElementById('document11').value == '') {
-                                                document.getElementById('document11-s').style.display = 'block';
-                                            } else {
-                                                return true;
-                                            }
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
     }
     return false;
 }
