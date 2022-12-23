2043351f22605d137aa3bedf6a97af564158f342
s1ghe <golikoefim65@gmail.com>
2022-12-23 13:31:58 +0300

    edd

--- app.js
+++ app.js
@@ -5,10 +5,13 @@ telegramAPI.expand();
 telegramAPI.MainButton.textColor = '#FFFFFF';
 telegramAPI.MainButton.color = '#198754';
 
+let dict_document = {
+    'document1': ''
+}
 
 function validate_from() {
     if (document.getElementById('document1').value != '') {
-        array_value.push(document.getElementById('document1').value);
+        dict_document.document1 = document.getElementById('document1').value
     }
     return false;
 }
@@ -17,5 +20,5 @@ telegramAPI.MainButton.setText('Отправить');
 telegramAPI.MainButton.show();
 
 Telegram.WebApp.onEvent('mainButtonClicked', function () {
-    telegramAPI.sendData(array_value);
+    telegramAPI.sendData(dict_document);
 });
