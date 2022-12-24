9bb781dc72923587dd0abf83b34294282cac1131
s1ghe <golikoefim65@gmail.com>
2022-12-24 13:02:27 +0300

    edd

--- app.js
+++ app.js
@@ -11,22 +11,29 @@ let dict_doc = []
 function validate_from() {
     let docs = document.getElementsByClassName('documents');
     let docs_howers = document.getElementsByClassName('documents-error');
-    for(let i = 0; i < docs.length; i++){
-        if(docs[i].value === '')
+    for (let i = 0; i < docs.length; i++) {
+        if (docs[i].value === '')
             docs_howers[i].style.display = 'block';
         else
             docs_howers[i].style.display = 'none';
         dict_doc[i] = docs[i].value;
     }
 
-    for(let j = 0; j < dict_doc.length; j++){
-        if (dict_doc[j] == '')
-            return false;
+    for (let j = 0; j < dict_doc.length; j++) {
+
+        if (dict_doc[j] == '') {
+            let trues = true;
+            break;
+        }
     }
-    telegramAPI.MainButton.setText('Отправить');
-    telegramAPI.MainButton.show();
+    if (trues === false) {
+        telegramAPI.MainButton.setText('Отправить');
+        telegramAPI.MainButton.show();
+
+    }
     return false;
 }
+
 Telegram.WebApp.onEvent('mainButtonClicked', function () {
     telegramAPI.sendData(`${dict_doc},`);
 });
