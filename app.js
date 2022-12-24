9f5f730ec1d2cc8c3dd24d0ddf9dac073829dc4b
s1ghe <golikoefim65@gmail.com>
2022-12-24 12:14:42 +0300

    edd

--- app.js
+++ app.js
@@ -13,18 +13,12 @@ function validate_from() {
         document.getElementById('document1-s').style.display = 'block';
     } else {
         dict_doc[0] = document.getElementById('document1').value;
+        telegramAPI.MainButton.setText('Отправить');
+        telegramAPI.MainButton.show();
     }
     return false;
 }
 
-telegramAPI.MainButton.setText('Отправить');
-telegramAPI.MainButton.show();
-
 Telegram.WebApp.onEvent('mainButtonClicked', function () {
-    if (document.getElementById('document1').value != ''){
-        dict_doc[0] = document.getElementById('document1').value;
-        telegramAPI.sendData(`${dict_doc},`);
-    }else {
-        telegramAPI.sendData('empty');
-    }
+    telegramAPI.sendData(`${dict_doc},`);
 });
